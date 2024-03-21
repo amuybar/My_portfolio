206dc06e26c250f6a423c9c1bac1eb95c7204678
amuybar <amuyunzubq@gmail.com>
2024-03-21 18:50:26 +0300

    Edited the Nav Bar, Added css to the links

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,7 +5,7 @@ import '../styles/Contact.css'
 
 function Contact() {
   return (
-    <div className="contact">
+    <div className="contact" id='contact'>
       <Contacts />
       <Form />
     </div>
